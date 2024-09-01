4e25350375396d3a2ea7e6360e8406b14ce36f4a
1cbyc <isaacnsisong@gmail.com>
2024-09-01 21:59:51 +0100

    building an image classification model for diseases with ml

--- src/model_training.py
+++ src/model_training.py
@@ -16,3 +16,5 @@ def build_model(input_shape=(224, 224, 3)):
     model.compile(optimizer=Adam(learning_rate=0.0001),
                   loss='binary_crossentropy',
                   metrics=['accuracy'])
+
+    return model
