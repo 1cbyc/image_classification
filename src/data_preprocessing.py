2dd344e7a98d32cfc216f094b6ceeb63dec0c52d
1cbyc <isaacnsisong@gmail.com>
2024-09-01 21:20:53 +0100

    building an image classification model for diseases with ml

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -4,5 +4,4 @@ import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 def preprocess_image(image_path, target_size=(224, 224)):
-    image = cv2.imread((image_path)
-                )
+    image = cv2.imread(image_path)
