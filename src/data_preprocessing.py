ee233faad0efe0ef998ee2ce56c8f56f355f9a84
1cbyc <isaacnsisong@gmail.com>
2024-09-01 21:41:41 +0100

    building an image classification model for diseases with ml

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -7,4 +7,5 @@ def preprocess_image(image_path, target_size=(224, 224)):
     image = cv2.imread(image_path)
     image = cv2.resize(image, target_size)
     image = image / 255.0 # for me to normalize the image
-    
+    return image
+
