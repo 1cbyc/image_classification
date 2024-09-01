45c180a0f0fdda83ad60230e06cfa8ce40ed4c54
1cbyc <isaacnsisong@gmail.com>
2024-09-01 22:07:15 +0100

    building an image classification model for diseases with ml

--- src/model_training.py
+++ src/model_training.py
@@ -24,7 +24,7 @@ def build_model(input_shape=(224, 224, 3)):
     return model
 
 def train_model(model, train_data_dir, val_data_dir, batch_size=32, epochs=10):
-    datagen = ImageDataGenerator(rescale=1.0/255)
+    datagen = ImageDataGenerator(rescale=1.0/255, validation_split)
 
 
 # what i did here is used the vgg16 model as a base and added custom layers on top for binary classification
