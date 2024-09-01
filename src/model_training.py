279f86d0f9112934f7c5cae4a123a4ccc963e6ed
1cbyc <isaacnsisong@gmail.com>
2024-09-01 21:57:18 +0100

    building an image classification model for diseases with ml

--- src/model_training.py
+++ src/model_training.py
@@ -4,4 +4,6 @@ from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.optimizers import Adam
 
 def build_model(input_shape=(224, 224, 3)):
-    base_model = VGG16(weights='imagenet', include_top=False, input_shape)
+    base_model = VGG16(weights='imagenet', include_top=False, input_shape=input_shape)
+
+    model
