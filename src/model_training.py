a919832582eb7b0d969f67b9a5b55d026c7b0300
1cbyc <isaacnsisong@gmail.com>
2024-09-01 22:05:25 +0100

    building an image classification model for diseases with ml

--- src/model_training.py
+++ src/model_training.py
@@ -1,9 +1,12 @@
 #to build the model i used this calls
-# from tensorflow.keras.applications import VGG16
+from tensorflow.keras.applications import VGG16
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.optimizers import Adam
 # to train the model, i added these calls
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import ModelCheckpoint
+
 
 def build_model(input_shape=(224, 224, 3)):
     base_model = VGG16(weights='imagenet', include_top=False, input_shape=input_shape)
