5e2dd963b2ecd2fe11dec603391cf9551f84c797
1cbyc <isaacnsisong@gmail.com>
2024-09-01 22:30:21 +0100

    building an image classification model for diseases with ml

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -3,4 +3,4 @@ from sklearn.metrics import  classification_report, confusion_matrix
 
 def plot_metrics(history):
     plt.plot(history.history['accuracy'], label='accuracy')
-    plt.plot(history.history['val_'])
+    plt.plot(history.history['val_accuracy'], label)
