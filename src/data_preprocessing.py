cc3efa18dfa202fedf6d2dd72985762e76ce5e56
1cbyc <isaacnsisong@gmail.com>
2024-09-01 21:47:58 +0100

    building an image classification model for diseases with ml

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -14,5 +14,6 @@ def data_augmentation():
         rotation_range=20,
         width_shift_range=0.2,
         height_shift_range=0.2,
-        shear_range=
+        shear_range=0.2,
+        zoom
     )
