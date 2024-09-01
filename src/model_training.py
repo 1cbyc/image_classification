9b57f0887c09f0d1dedf331d44ca7cca327c8688
1cbyc <isaacnsisong@gmail.com>
2024-09-01 21:55:58 +0100

    building an image classification model for diseases with ml

--- src/model_training.py
+++ src/model_training.py
@@ -2,3 +2,4 @@ from tensorflow.keras.applications import VGG16
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.optimizers import Adam
+
