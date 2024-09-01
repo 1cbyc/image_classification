2b515257ccd2ba72bb1b390313f9347aee1fbb78
1cbyc <isaacnsisong@gmail.com>
2024-09-01 23:00:26 +0100

    building an image classification model for diseases with ml

--- train_and_evaluate.py
+++ train_and_evaluate.py
@@ -4,4 +4,6 @@ from src.model_evaluation import plot_metrics, evaluate_model
 # first name paths to data dir
 train_data_dir = 'data/train'
 val_data_dir = 'data/val'
-test_data_dir = 'data'
+test_data_dir = 'data/test'
+
+# then building the model
