688e79e3467705e750029101ac7020af519db254
1cbyc <isaacnsisong@gmail.com>
2024-09-02 20:27:58 +0100

    building an image classification model for diseases with ml

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -33,4 +33,10 @@ def evaluate_model(model, test_data_generator):
 
     plt.figure()
     plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc:.4f})')
-    
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
+
