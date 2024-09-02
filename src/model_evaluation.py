abc187487c730455216421dafa7bf63f0791d666
1cbyc <isaacnsisong@gmail.com>
2024-09-02 13:43:10 +0100

    building an image classification model for diseases with ml

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -7,4 +7,5 @@ def evaluate_model(model, test_data_generator):
     y_pred = model.predict(test_data_generator)
     y_true = test_data_generator.classes
 
-    # then convert predictions to binary labels (
+    # then convert predictions to binary labels (let's assume it is binary classification)
+    
