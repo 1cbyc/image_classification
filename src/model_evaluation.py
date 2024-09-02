d010458f6197cead378577b94c35ae8a76206b9d
1cbyc <isaacnsisong@gmail.com>
2024-09-02 13:41:35 +0100

    building an image classification model for diseases with ml

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -2,4 +2,4 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, roc_curve, auc
 import numpy as np
 
-def evaluate_model(model)
+def evaluate_model(model, test_data_genera)
